d875ac3e74bcb10cde479c6f3159ac7e87d10ad8
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-04-01 14:33:04 -0300

    Add module pattern to solve DI problem

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function cacheTranslations(options) {
 
 function wrapTranslations(options) {
   return es.map(function(file, callback) {
-    file.contents = new Buffer(gutil.template('angular.module("<%= module %>"<%= standalone %>).config(["$translateProvider", function($translateProvider) {\n<%= contents %>}]);\n', {
+    file.contents = new Buffer(gutil.template('(function construct(angular) {"use strict";angular.module("<%= module %>"<%= standalone %>).config(["$translateProvider", function($translateProvider) {\n<%= contents %>}]);\n})(angular);', {
       contents: file.contents,
       file: file,
       module: options.module || 'translations',
